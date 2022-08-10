c49a0133cca6d1f4f1c3216e2b005fef0c8b264d
HugoAH <hugo.ah-nieme@estaca.eu>
2022-08-10 17:33:32 +0200

    Remove of Debug files

--- Software/ECU_F411/Debug/Core/Src/ignition.d
+++
@@ -1,4 +0,0 @@
-Core/Src/ignition.o: ../Core/Src/ignition.c ../Core/Inc/ignition.h \
- ../Core/Inc/interpolation.h
-../Core/Inc/ignition.h:
-../Core/Inc/interpolation.h:
--- Software/ECU_F411/Debug/Core/Src/injection.d
+++
@@ -1,4 +0,0 @@
-Core/Src/injection.o: ../Core/Src/injection.c ../Core/Inc/injection.h \
- ../Core/Inc/interpolation.h
-../Core/Inc/injection.h:
-../Core/Inc/interpolation.h:
